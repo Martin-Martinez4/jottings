ab1f70f57c1d5f0a15ceb831a49a9f595dc86ac3
MartM <martmartez4@gmail.com>
2022-07-08 18:58:06 -0700

    - Make home page dynamic

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,18 +1,63 @@
 
-import { useEffect } from "react";
+import { MouseEvent, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { StateType } from "../../types/project.type";
 import { HomeContentContainer, ProjectContainer, ProjectInforamtionContainer } from "./home.styles";
 import { SiginPageTopNav, TopNavRight } from "../signin/Signin.styles";
 import { TransparentButton } from "../../global.style";
 import LogoSvg from "../../component/Svg_Icons/Logo/Logo._svg";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { getProject } from "../../actions/projectSlice";
 
 const Home = () => {
 
-    const user = useSelector((state:StateType) => state.auth)
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const user = useSelector((state:StateType) => state.auth);
+
+    const [userProjects, setUserProjects] = useState<JSX.Element[]
+    >([]);
+
+    function handleGoToProject(e: MouseEvent<HTMLDivElement, globalThis.MouseEvent>){
+
+        e.preventDefault();
+
+        const el = e.target as HTMLDivElement
+
+        const project_id = el.getAttribute("data-id");
+
+        dispatch(getProject(project_id));
 
 
-    useEffect(() => {console.log("user", user)}, [user])
+
+        
+
+    }
+
+
+    useEffect(() => {
+        
+        let tempProjects: JSX.Element[] = user.projects.map(project => {
+
+            return (
+                    <ProjectInforamtionContainer key={`project_${project._id.toString()}`} data-id={project._id.toString()} onClick={(e) => handleGoToProject(e)}>
+                        <p>{project?.logo_url? project?.logo_url.toString() : ""}</p>
+                        <p>{project.title? project.title.toString(): ""}</p>
+                        <p>{project.description.toString()}</p>
+                    </ProjectInforamtionContainer>
+                
+
+            )
+        })
+
+        setUserProjects([...tempProjects])
+        
+    }, [user])
+
+
+    useEffect(() => {console.log("user", user, "userProjects", userProjects)}, [user])
 
     return(
 
@@ -26,56 +71,18 @@ const Home = () => {
             <HomeContentContainer>
                 <div>
                     <p>Profile Pic</p>
-                    <p>user Name</p>
+                    <p>{user.username}</p>
                 </div>
                 <div>
-                    <h3>Team Name</h3>
+                    <h3>{user.username}'s project</h3>
                     <ProjectContainer>
-                        <ProjectInforamtionContainer>
+                       { userProjects}
+                        {/* <ProjectInforamtionContainer>
                             <p>Logo</p>
                             <p>Project Name</p>
                             <p>description: Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis quisquam amet quae ipsa nihil ut doloribus quasi velit fugiat consequatur.</p>
-                        </ProjectInforamtionContainer>
-                        <ProjectInforamtionContainer>
-                            <p>Logo</p>
-                            <p>Project Name</p>
-                            <p>description: Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis quisquam amet quae ipsa nihil ut doloribus quasi velit fugiat consequatur.</p>
-                        </ProjectInforamtionContainer>
-                        <ProjectInforamtionContainer>
-                            <p>Logo</p>
-                            <p>Project Name</p>
-                            <p>description: Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis quisquam amet quae ipsa nihil ut doloribus quasi velit fugiat consequatur.</p>
-                        </ProjectInforamtionContainer>
-                        <ProjectInforamtionContainer>
-                            <p>Logo</p>
-                            <p>Project Name</p>
-                            <p>description: Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis quisquam amet quae ipsa nihil ut doloribus quasi velit fugiat consequatur.</p>
-                        </ProjectInforamtionContainer>
-                        <ProjectInforamtionContainer>
-                            <p>Logo</p>
-                            <p>Project Name</p>
-                            <p>description: Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis quisquam amet quae ipsa nihil ut doloribus quasi velit fugiat consequatur.</p>
-                        </ProjectInforamtionContainer>
-                        <ProjectInforamtionContainer>
-                            <p>Logo</p>
-                            <p>Project Name</p>
-                            <p>description: Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis quisquam amet quae ipsa nihil ut doloribus quasi velit fugiat consequatur.</p>
-                        </ProjectInforamtionContainer>
-                        <ProjectInforamtionContainer>
-                            <p>Logo</p>
-                            <p>Project Name</p>
-                            <p>description: Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis quisquam amet quae ipsa nihil ut doloribus quasi velit fugiat consequatur.</p>
-                        </ProjectInforamtionContainer>
-                        <ProjectInforamtionContainer>
-                            <p>Logo</p>
-                            <p>Project Name</p>
-                            <p>description: Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis quisquam amet quae ipsa nihil ut doloribus quasi velit fugiat consequatur.</p>
-                        </ProjectInforamtionContainer>
-                        <ProjectInforamtionContainer>
-                            <p>Logo</p>
-                            <p>Project Name</p>
-                            <p>description: Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis quisquam amet quae ipsa nihil ut doloribus quasi velit fugiat consequatur.</p>
-                        </ProjectInforamtionContainer>
+                        </ProjectInforamtionContainer> */}
+                       
 
                     </ProjectContainer>
                 </div>
